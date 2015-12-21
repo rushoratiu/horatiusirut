be8afd145a84a6b2bd33586bda165a7dd0e6646b
rushoratiu <rushoratiu3@gmail.com>
2015-12-21 22:49:47 +0200

    3

--- Site eu si Rut/poze.php
+++ Site eu si Rut/poze.php
@@ -6,9 +6,14 @@ include'includes/overall/header.php';
 	<br>
 	<p>Check them out </p><br>
 	<h4>Cea mai recenta si de altfel cea mai reusita poza (la ski <a href="http://www.superskicavnic.ro/">Cavnic</a>) #teiubesc</h4>
-	<img src="css/images/ski.jpg " id="ski">
+	<img src="css/images/ski.jpg " class="ski">
 	<br><br>
-	
+	<h4>Fiind fericit cu cea mai scumpa dintre toate !</h4>
+	<img src="css/images/lan.jpg" class="ski">
+	<h4>In acel moment vom incepe impreuna aventura vietii noastre ! OFICIAL !! <a href="#">#bestdayever</a></h4>
+	<img src="css/images/logodna.jpg" class="ski">
+	<h4>“your hand touching mine. this is how galaxies collide.”</h4>
+	<img src="css/images/hands.jpg" class="ski">
 
 	
 <?php include'includes/overall/footer.php'; ?>
